d5936090ece45b3a86d907ae5808bc3e6920efca
mrobi27 <robiardita5@gmail.com>
2025-06-01 20:01:21 +0700

    Menambahkan controller, main, resource, view

---
+++ Final_Project_PBO_Team-Dash/src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static Stage primaryStage; // agar bisa diakses dari controller
+    public static Scene mainScene;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Load halaman awal (Login.fxml)
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        mainScene = new Scene(root);
+
+        primaryStage = stage;
+        primaryStage.setTitle("UMM Library Access");
+        primaryStage.setScene(mainScene);
+
+        primaryStage.show();
+    }
+
+}
